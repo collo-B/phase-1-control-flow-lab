2524b4dc3f9176914c80e41e3043b3eb94d23051
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-06-06 19:49:08 +0300

    commit tree 58841f5279125dd0fd95328ac162076b81ce3730

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let result
+  if (feet <= 400) {
+    result = 'This one is on me!';
+  }
+  else if (feet > 2000 && feet <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if (feet > 2500) {
+    result = 'No can do.';
+  }
+  return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+  let result
+  if (city == 'NYC'){
+    result = 'Ok, sounds good.';
+  }
+  
+  else {
+    result = 'No go.';
+  }
+  return result
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let result
+if (tip=='generous'){
+result = 'Thank you so much.'
+}
+else if (tip=='not as generous'){
+result = 'Thank you.'
+}
+else {
+  result = 'Bye.'
+}
+return result
 }
